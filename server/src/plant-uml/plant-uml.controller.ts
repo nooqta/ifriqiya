b20fc371dd23b1cba990647e763c97ca0e1067a6
Anis Marrouchi <anismarrouchi@hotmail.com>
2019-10-31 17:54:42 +0100

    fix: comment plantuml

--- server/src/plant-uml/plant-uml.controller.ts
+++ server/src/plant-uml/plant-uml.controller.ts
@@ -1,19 +1,18 @@
 import { Controller, Res, Get, Post, Body } from '@nestjs/common';
 import * as fs from 'fs';
 import { Request, Response } from 'express';
-import * as plantuml from 'node-plantuml';
+//import * as plantuml from 'node-plantuml';
 
 @Controller('plant-uml')
 export class PlantUmlController {
     @Get('generate')
     generate(@Res() res: Response) {
-        //const data = JSON.stringify(models);
-        plantuml.useNailgun(); // Activate the usage of Nailgun
+        // plantuml.useNailgun(); // Activate the usage of Nailgun
         
-        const gen = plantuml.generate('dist/browser/assets/test.wsd');
-        const file = gen.out.pipe(fs.createWriteStream('dist/browser/assets/output-file.png'));
-        res.header('Content-Type', 'image/png');
-        return fs.readFileSync(`dist/browser/assets/output-file.png`);
+        // const gen = plantuml.generate('dist/browser/assets/test.wsd');
+        // res.header('Content-Type', 'image/png');
+        // gen.out.pipe(fs.createWriteStream('dist/browser/assets/output-file.png'));
+        res.send({uml: 'Uml :)'});
 
     }
 }
